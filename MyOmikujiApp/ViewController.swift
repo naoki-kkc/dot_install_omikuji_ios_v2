5cea02e3ac7f8b9dedffeaf783a8eea41c9ad077
naoki-kkc <naoki2kuchi@gmail.com>
2021-03-09 22:31:47 +0900

    chapter 06 end

--- MyOmikujiApp/ViewController.swift
+++ MyOmikujiApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var myLabel: UILabel!
     
     @IBAction func getOmikuji(_ sender: Any) {
-        let results = ["大吉", "中吉", "大凶"]
+        let results = ["大吉", "中吉", "吉", "凶", "大凶"]
         let random = arc4random_uniform(UInt32(results.count))
         myLabel.text = results[Int(random)]
     }
@@ -21,6 +21,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        myLabel.layer.borderColor = UIColor.orange.cgColor
+//        myLabel.layer.borderWidth = 5
+        myLabel.layer.cornerRadius = myLabel.bounds.width / 2
+        myLabel.layer.masksToBounds = true
     }
 
 
