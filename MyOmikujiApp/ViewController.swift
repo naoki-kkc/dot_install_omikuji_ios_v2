9d8d7ae2ea37db6cce32a3a264c69889bf269613
naoki-kkc <naoki2kuchi@gmail.com>
2021-03-09 22:09:44 +0900

    Initial Commit

---
+++ MyOmikujiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyOmikujiApp
+//
+//  Created by ginger893 on 2021/03/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
