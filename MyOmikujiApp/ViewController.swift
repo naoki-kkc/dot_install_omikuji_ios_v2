83bbb14b640f0fd3016901e3a0073c89a01170cd
naoki-kkc <naoki2kuchi@gmail.com>
2021-03-09 22:24:05 +0900

    chapter 03 end

--- MyOmikujiApp/ViewController.swift
+++ MyOmikujiApp/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBAction func getOmikuji(_ sender: Any) {
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
